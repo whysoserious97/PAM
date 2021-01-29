8a0e6427502fe5cf3c73d9ae665af342f68ec789
whysoserious97 <andrei.lesco@isa.utm.md>
2021-01-29 20:04:44 +0200

    Added back behaviour

--- app/src/main/java/com/example/pam_lab1/Activity2.kt
+++ app/src/main/java/com/example/pam_lab1/Activity2.kt
@@ -1,32 +1,32 @@
 package com.example.pam_lab1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_2.*
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+
 
 class Activity2 : AppCompatActivity() {
 
-    companion object{
-        const val Total_Count = "total_count"
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
-        showRandom()
+        showInput()
     }
-    fun showRandom(){
-        val count = intent.getIntExtra(Total_Count, 0)
+    fun showInput(){
+        val received = intent.getStringExtra("input").toString()
+        act2_input.setText(received)
 
-        val random = Random()
-        var randomInt = 0
+    }
 
-        if (count>0){
-            randomInt = random.nextInt(count + 1)
-        }
-        val textViewRandom = findViewById<TextView>(R.id.textView2)
-        textViewRandom.text = randomInt.toString();
-        val textViewLabel = findViewById<TextView>(R.id.textViewLabel)
-        textViewLabel.text = getString(R.string.random_heading,count)
+    fun callback(view: View) {
+        val input = act2_input.text.toString()
+        val intent = Intent(this,MainActivity::class.java)
+        intent.putExtra("back",input)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/pam_lab1/MainActivity.kt
+++ app/src/main/java/com/example/pam_lab1/MainActivity.kt
@@ -4,31 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_2.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(intent.hasExtra("back")){
+            displayInput()
+        }
     }
-    fun updateNumber(view: View){
-        val textView = findViewById<TextView>(R.id.number)
-        val countString = textView.text.toString()
-        var count : Int = Integer.parseInt(countString)
-        count++
-        textView.text = count.toString()
+    fun displayInput(){
+        val received = intent.getStringExtra("back").toString()
+        returned.setText(received)
     }
-    fun randomMe(view: View){
-        val randomIntent = Intent(this,Activity2::class.java)
-
-        val textView = findViewById<TextView>(R.id.number)
-        val countString = textView.text.toString()
-
-        val count = Integer.parseInt(countString)
-
-        randomIntent.putExtra(Activity2.Total_Count,count)
-
-
-        startActivity(randomIntent)
+    fun next(view: View){
+        val input = inputText.text.toString()
+        val intent = Intent(this,Activity2::class.java)
+        intent.putExtra("input",input)
+        startActivity(intent)
     }
 }
