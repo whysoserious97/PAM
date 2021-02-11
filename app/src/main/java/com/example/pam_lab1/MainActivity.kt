8ef5eaf770915713fa6ceddb529c4cfa491840e4
whysoserious97 <andrei.lesco@isa.utm.md>
2021-02-11 18:18:34 +0200

    on Destroy

--- app/src/main/java/com/example/pam_lab1/MainActivity.kt
+++ app/src/main/java/com/example/pam_lab1/MainActivity.kt
@@ -6,10 +6,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_2.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(getApplicationContext(),"Come back later", Toast.LENGTH_SHORT).show();
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
