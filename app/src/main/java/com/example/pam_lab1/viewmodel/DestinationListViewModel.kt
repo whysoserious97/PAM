d9b0c611cb99cc0cf93ade11fd6877c51e9fae8e
whysoserious97 <andrei.lesco@isa.utm.md>
2021-04-21 08:49:42 +0300

    Success on loading taskslist

--- app/src/main/java/com/example/pam_lab1/viewmodel/DestinationListViewModel.kt
+++ app/src/main/java/com/example/pam_lab1/viewmodel/DestinationListViewModel.kt
@@ -9,21 +9,11 @@ import com.example.pam_lab1.repository.DestinationRepository
 class DestinationListViewModel : ViewModel() {
 
     var servicesLiveData: MutableLiveData<List<Destination>>? = null
-    var destinationLiveData: MutableLiveData<Destination>? = null
-    var addDestination : MutableLiveData<Destination>? = null
 
     fun getDestinations(filter: HashMap<String,String>) : LiveData<List<Destination>>? {
         //val filter = HashMap<String, String>()
         servicesLiveData = DestinationRepository.getServicesApiCall(filter)
         return servicesLiveData
-    }
-    fun getDestination(id: Int) : LiveData<Destination>? {
-        destinationLiveData = DestinationRepository.loadDetails(id)
-        return destinationLiveData
-    }
-    fun addDestination(destination: Destination) : LiveData<Destination>? {
-        addDestination = DestinationRepository.addDestination(destination)
-        return addDestination
     }
 
 }
